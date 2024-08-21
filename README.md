995a99beec7d9d64225ff517f4546bb20b9d90aa
MoGamal27 <113600854+MoGamal27@users.noreply.github.com>
2024-08-21 14:29:38 +0300

    update

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # Center Solution Book
 
 ## Table of Contents
@@ -11,6 +12,8 @@
 ## Overview
 
 The center solution system is designed to manage various aspects of a service center with multiple branches. It includes features for online registration, employee management, service offerings, reservations, and user management. The system also incorporates privilege levels to ensure secure access control.
+
+
 
 ## Features
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BranchModule } from './branch/branch.module';
+import { EmployeeModule } from './employee/employee.module';
+import { PaymentModule } from './payment/payment.module';
+import { ClientModule } from './client/client.module';
+import { TimeFrameModule } from './timeframe/timeframe.module';
 import { AuthModule } from './auth/auth.module';
-import { DatabaseModule } from './config/databaseConfig';
-
 @Module({
-  imports: [AuthModule, DatabaseModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345',
+      database: 'Easy-book',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Set to false in production
+    }),
+    BranchModule,
+    EmployeeModule,
+    PaymentModule,
+    ClientModule,
+    TimeFrameModule,
+    AuthModule
+  ],
 })
 export class AppModule {}
-
